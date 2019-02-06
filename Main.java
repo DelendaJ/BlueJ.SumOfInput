7b3a812349214584c2795d139bc3dc2858f0b2c5
Delenda Joseph <delendajoseph@zipcoders-MacBook-Pro.local>
2019-02-06 18:12:19 -0500

    Summy sum sum

--- Main.java
+++ Main.java
@@ -1,9 +1,29 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        int sum = 0; 
+        for(int i = 1; i <= number; ++i)
+  {     sum += i;
+        System.out.println("Sum = " + sum);
     }
 }
+        }
+
+
+        
+        
+        
+      
+        
+        
+        
+        
+
+ 
